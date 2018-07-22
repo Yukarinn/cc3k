c29061c781b94f26d557f5605bda0e6a400e0d3d
Yukarin <ripple-495@outlook.com>
2018-07-22 17:02:26 -0400

    merged floor to floor2

--- floor2.h
+++ floor2.h
@@ -9,24 +9,27 @@ class Player;
 
 class Floor {
     std::vector<std::vector<Cell*>> theFloor;
-		Player * player;
-		std::vector<std::vector<int>> layout;
-		public:
-			Floor(std::vector<std::vector<char>> plan); // spawn everything
-			~Floor();
-			std::string draw();
-			void setPlayer(Player* player);
-			Player* getPlayer();
-	
-			void spawnPlayer();
-			void spawnStairs();
-			void spawnPotions();
-			void spawnGold();
-			void spawnEnemies();
-		
-			void setup();
-		private:
-			void floodfill(int i, int j, int chamber);
+    Player * player;
+    std::vector<std::vector<int>> layout;
+    std::vector<vector<Cell *>> chambers;
+    std::vector<Enemy *> mobs;
+public:
+    Floor(std::vector<std::vector<char>> plan); // spawn everything
+    ~Floor();
+    std::string draw();
+    void setPlayer(Player* player);
+    Player* getPlayer();
+    void mobAct();
+    
+    void spawnPlayer();
+    void spawnStairs();
+    void spawnPotions();
+    void spawnGold();
+    void spawnEnemies();
+    
+    void setup();
+private:
+    void floodfill(int i, int j, int chamber);
 };
 
 #endif
