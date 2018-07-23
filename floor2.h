bc83f883dd1317510b5c1baa81146c671612964e
Yukarin <ripple-495@outlook.com>
2018-07-22 20:48:24 -0400

    floor with spawn enemy commented out

--- floor2.h
+++ floor2.h
@@ -6,18 +6,22 @@
 
 class Cell;
 class Player;
+class Enemy;
 
 class Floor {
     std::vector<std::vector<Cell*>> theFloor;
     Player * player;
     std::vector<std::vector<int>> layout;
     std::vector<std::vector<Cell *>> chambers;
+    std::vector<Enemy *> mobs; 
     
     void spawnPlayer(Player * thePlayer, int chamberNum);
     void spawnStairs(int chamberNum);
     void spawnPotions();
     void spawnGold();
     void spawnEnemies();
+    
+    void mobAct();
     
 public:
     Floor(std::vector<std::vector<char>> plan);
