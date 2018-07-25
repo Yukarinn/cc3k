70b76174ffab4cbcf64526cbaf49e28d1bb6d74f
Yukarin <ripple-495@outlook.com>
2018-07-25 15:26:38 -0400

    added comments (potion)

--- potion.cc
+++ potion.cc
@@ -11,28 +11,32 @@ Potion::Potion(PotionType potionType): Item(ObjectType::Potion, 'P'), potionType
 Potion::~Potion() {}
 
 PotionType Potion::getPotionType() const {
-	return potionType;
+    return potionType;
 }
 
 string Potion::getEffect() const {
-	if (find(Potion::seen.begin(), Potion::seen.end(), potionType) == Potion::seen.end())
-		return "Unknown Potion";
-	switch(potionType) {
-		case PotionType::RH:
-			return "Plus HP";
-		case PotionType::BA:
-			return "Buff ATK";
-		case PotionType::BD:
-			return "Buff DEF";
-		case PotionType::PH:
-			return "Minus HP";
-		case PotionType::WA:
-			return "Debuff ATK";
-		case PotionType::WD:
-			return "Debuff DEF";
-	}
+    // check if the player has seen this potion before
+    // if not, tell the player it's unknown potion
+    if (find(Potion::seen.begin(), Potion::seen.end(), potionType) == Potion::seen.end())
+        return "Unknown Potion";
+    // otherwise, return the type of the potion
+    switch(potionType) {
+        case PotionType::RH:
+            return "Plus HP";
+        case PotionType::BA:
+            return "Buff ATK";
+        case PotionType::BD:
+            return "Buff DEF";
+        case PotionType::PH:
+            return "Minus HP";
+        case PotionType::WA:
+            return "Debuff ATK";
+        case PotionType::WD:
+            return "Debuff DEF";
+    }
 }
 
 void Potion::see(PotionType potionType) {
-	Potion::seen.emplace_back(potionType);
+    // add the new potion that player used too the seen list
+    Potion::seen.emplace_back(potionType);
 }
--- potion.h
+++ potion.h
@@ -5,18 +5,20 @@
 #include "item.h"
 
 enum class PotionType {
-	RH, BA, BD, PH, WA, WD
+    RH, BA, BD, PH, WA, WD
 };
 
 class Potion: public Item {
-	PotionType potionType;
-	static std::vector<PotionType> seen;
-	public:
-		Potion(PotionType potionType);
-		~Potion();
-		PotionType getPotionType() const;
-		std::string getEffect() const;
-		static void see(PotionType potionType);
+    PotionType potionType;
+    // the list of seen potions
+    static std::vector<PotionType> seen;
+public:
+    Potion(PotionType potionType);
+    ~Potion();
+    PotionType getPotionType() const;
+    std::string getEffect() const;
+    // if the potion is used, player can see it
+    static void see(PotionType potionType);
 };
 
 #endif
